9d53c210cd7b8ff727c776b2b582d304c737ac7e
Corey Sawin <sawincp@gmail.com>
2023-03-02 11:46:39 -0500

    commit tree 02aee0e88168c4103f9e46d6e25ac7995042a92e

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const[addItem, setAddItem]= useState(false)
+  
+  function handleAddToCart(){
+      setAddItem((addItem)=> !addItem)
+  }
+
+  const itemClass = addItem ? "in-cart": ""
+  
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className= {addItem ? "remove": "add"} onClick={handleAddToCart}>
+        {addItem ? "Remove from Cart": "Add to Cart" }
+      </button>
     </li>
   );
 }
